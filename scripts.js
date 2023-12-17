3d1077f82ce063f8b7444024001dd188548ae5ac
Lane Buckler <141290331+Lanestuartbuckler@users.noreply.github.com>
2023-12-17 17:40:51 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,28 @@
+const runner = document.querySelector("[data-runner]");
+const hurdle = document.querySelector("[data-hurdle]")
+const StartScreenElement = document.querySelector("[data-startscreen]")
+
+function jump() {
+    if (runner.classList != "jump") {
+        runner.classList.add("jump");
+
+        setTimeout(function () {
+            runner.classList.remove("jump");
+        }, 300);
+    }
+}
+
+let stillRunning = setInterval(function () {
+    let runnerTop = parseInt(window.getComputedStyle(runner).getPropertyValue("top"));
+    
+    let hurdleLeft = parseInt(window.getComputedStyle(hurdle).getPropertyValue("left"));
+
+    if (hurdleLeft < 20 && hurdleLeft > 0 && runnerTop >= 80) {
+        alert("You hit a hurdle! You have been disqualified!")
+        location.reload(true)
+    }
+}, 10);
+
+document.addEventListener("keydown", function (event) {
+    jump();
+});
